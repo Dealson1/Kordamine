ef8bdc6110c6192ba00db12a3a7a4d6c2d957e88
Alexey <alexborn950@gmail.com>
2022-01-10 16:15:20 +0200

    1

--- Program.cs
+++ Program.cs
@@ -10,7 +10,23 @@ namespace Kordamine
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("1 - Подсчет символов в строке./ 2 - 2 Задание/ 3 - 3 Задание/ 4 - Выход");
+            bool x = true;
+            while (x)
+            {
+                int t = int.Parse(Console.ReadLine());
 
+                switch (t)
+                {
+                    case 1:
+                        SubClass.CharInText();
+                        break;
+                    default:
+                        Console.WriteLine("Такого пункта нет.");
+                        break;
+                }
+            }
         }
     }
 }
